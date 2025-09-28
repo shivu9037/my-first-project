aae2d6192431cfe7293418fd44b8f2bb3eea9c5f
shivu9037 <shivasubramanya7566@gmail.com>
2025-09-28 18:59:42 +0530

    Updated calculator code

---
+++ main.py
@@ -0,0 +1,6 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print("sum is:",a+b)
+print("difference is:",a-b)
+print("multiplication is:",a*b)
+print("division is:",a/b)
